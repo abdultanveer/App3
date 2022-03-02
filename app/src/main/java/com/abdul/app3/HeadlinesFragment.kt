97a0d2cf96497d97855a0d5dd6281eaa805dde52
abdul <abdul.tanveer@gmail.com>
2022-03-02 22:41:48 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/app3/HeadlinesFragment.kt
@@ -0,0 +1,39 @@
+package com.abdul.app3
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+//to extend in java we use extends, to implement a interface we use implements
+class HeadlinesFragment : Fragment(), AdapterView.OnItemClickListener {
+    lateinit var headlinesListview: ListView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        var layout: View = inflater.inflate(R.layout.fragment_headlines, container, false)
+        headlinesListview = layout.findViewById(R.id.headlinesListview)
+        headlinesListview.setOnItemClickListener(this)
+        return layout
+    }
+
+    override fun onItemClick(adapter: AdapterView<*>?, row: View?, position: Int, rowId: Long) {
+        var item  = adapter?.getItemAtPosition(position).toString()
+        //Toast.makeText(activity, item, Toast.LENGTH_SHORT).show()
+        displayToast(item)
+    }
+
+    fun Fragment.displayToast( message: String){
+        Toast.makeText(activity,"extension function --"+message,Toast.LENGTH_SHORT).show()
+    }
+
+}
